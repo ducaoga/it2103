72ad88edb098bfa7c97d352e5a3471779db1ad17
salazarfl <floramariesalazar15@gmail.com>
2016-09-27 14:14:19 +0800

    span has been added

--- home.php
+++ home.php
@@ -3,7 +3,9 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-	<button class='btn btn-danger'>WELCOME MAE sweety</button>
+	<button class='btn btn-danger'>WELCOME MAE sweety
+		<span class='glyphicon glyphicon-user'></span>
+	</button>
 
 
 </body>
